fd2f73ea8d5ef110b002dc6c57740d6ff640c5f5
gleboss-redfab <gleb.v@redfab.ru>
2023-05-23 11:11:40 +0700

    Refactoring

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,31 +1,13 @@
 #!/usr/bin/env python3
-import random
-
-import prompt
-
+from brain_games.brain_even import start_game
 from brain_games.cli import welcome_user
 
 
 def main():
     """ user"""
     print("Welcome to the Brain Games!")
-    name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    for i in range(3):
-        number = random.randint(1, 100)
-        print(f'Question: {number}')
-
-        answer = prompt.string('Your answer: ')
-        if (number % 2 == 0 and answer == 'yes') or \
-           (number % 2 != 0 and answer == 'no'):
-            print('Correct!')
-            print(f'Congratulations, {name}!')
-        else:
-            correct_answer = 'yes' if number % 2 == 0 else 'no'
-            print(f"'{answer}' is wrong answer ;(. \
-                Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            return
+    username = welcome_user()
+    start_game(username)
 
 
 if __name__ == "__main__":
